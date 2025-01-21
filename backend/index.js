44700c01b001554611400756c20a7b82e8227d98
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2025-01-21 18:04:18 +0000

    create-user-model

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
+app.use("/api/users",userRoutes)
+
 app.get("/", (req, res)=> {
     res.send("Hello motto!")
 })
