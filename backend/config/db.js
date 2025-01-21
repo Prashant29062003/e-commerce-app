49b2aa0193983840e09e1b4d775124d005c32833
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2025-01-21 17:37:55 +0000

    intial-setup-frontend-backend

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Successfully connected to mongoDB :)`);
+        
+        
+    } catch (error) {
+        console.log(`ERROR ${error}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB
